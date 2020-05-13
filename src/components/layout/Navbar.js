86130d2da1060152bbcbff08ed80a8c0a875635b
Yelena Chamanova <ychamanova@gmail.com>
2020-05-12 18:45:42 -0700

    add ProjectList, ProjectSummary

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper blue lighten-1">
+            <div className="container">
+                <Link to='/' className="brand-logo">Project Management Tool</Link>
+                <SignedInLinks />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
