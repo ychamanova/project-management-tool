994276d16588b1f51e2f3ccb8c6910d74e03a7b6
Yelena Chamanova <ychamanova@gmail.com>
2020-05-25 16:22:44 -0700

    update navigation and styles

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProjectSummary = () => (
-    <div className="card z-depth-0 project-summary">
+    <div className="card z-depth-0 project-summary indigo lighten-5 center-align">
         <div card-content grey-text text-darken-3>
             <span className="card-title">Project Title</span>
             <p>Posted by the Net Ninja</p>
